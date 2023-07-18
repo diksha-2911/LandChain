950d6785ac8f1890f67be9416aa2255057e56ecf
diksha-2911 <90517961+diksha-2911@users.noreply.github.com>
2023-07-18 18:39:13 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/DjutxuoIWIjwNTDs8LtKDQDg32Iy0Zjh",
+      accounts: [ae940473ae35ff07c7e7c7d1d3901a0b3f6562aed5c0e7b787a0a1e19e85f056]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
